6e58183e959bbc30b09a3fe645aab1a29728ae76
Blahclah <adrianswindle@amdg.rockhusths.edu>
2020-03-05 09:48:02 -0600

    Table

---
+++ Table.py
@@ -0,0 +1,18 @@
+from ShuffleMachine import ShuffleMachine
+from Player import Player
+
+class Table(object):
+
+    def __init__(self):
+        self.create()
+
+    
+
+    def bets(self):
+        bet = []
+        for i in range(numPlayers)
+            bat = int(input("Enter player" + i +"'s bet "
+            bet.append(bat)
+            
+            
+    
